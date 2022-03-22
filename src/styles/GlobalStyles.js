73d7bb91ae5743a29a1e49033aefbfc5088b479d
sana0803 <sana0803@naver.com>
2022-03-22 16:01:25 +0900

    🎉 initial commit

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,11 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
-    //style reset
-    ${reset}
+	//style reset
+	${reset}
 
-    :root {
-    }
+	body {
+    letter-spacing: -0.02em;
+    background-color: var(--light-gray);
+		color: #333;
+		font-family: 'Pretendard Variable';
+  }
+
+	:root {
+	}
 `;
 
 export default GlobalStyles;
